a40b0e2850ca06b15d072aab4432f2719e0c33e2
Dhruv07Maurya <2022.dhruv.maurya@ves.ac.in>
2025-01-09 10:30:31 -0800

    WebDev

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,8 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 
 const App = () => {
-  return (
-    <div>
+  return <div>welcome!!</div>;
+};
 
-    </div>
-  )
-}
-
-export default App
+export default App;
